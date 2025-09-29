53304f73d263f81aff3060cda7719b09b10fe9ce
Rekhta Menahil <rekhta674@gmail.com>
2025-09-29 21:26:35 +0500

    add new rule to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,11 @@ module.exports = {
         "no-console": "error",
         "dot-notation": "error",
         "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/no-misused-promises": [
+            "error",
+            {
+                checksVoidReturn: false,
+            },
+        ],
     },
 };
